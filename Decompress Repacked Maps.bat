b2f6e871beae160ffbf245fbd8c350ba15d72e03
Thespikedballofdoom <43627353+Thespikedballofdoom@users.noreply.github.com>
2022-05-29 17:06:08 -0400

    actually fucking working now, fuck you valve

--- Decompress Repacked Maps.bat
+++ Decompress Repacked Maps.bat
@@ -1,4 +1,13 @@
 @echo off
 TITLE BSPZIP -REPACK (Files can be dragged onto this instead of typing)
 set /P gamer=Enter map file location (you can just drag map onto window): 
-"C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\bin\bspzip.exe" -repack %gamer%
+echo Ready to crash.
+set filepath=%gamer%
+for /F "delims=" %%i in (%filepath%) do set dirname="%%~dpi" 
+for /F "delims=" %%i in (%filepath%) do set filename="%%~nxi"
+echo %dirname%
+echo %filename%
+move %gamer% "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\bin"
+"C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\bin\bspzip.exe" -game "C:\Program Files (x86)\Steam\steamapps\common\Team Fortress 2\tf" -repack %filename%
+move %filename% %dirname%
+pause
